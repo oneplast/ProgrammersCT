8c651213fbb4906901ad6a479ed6195d9fcdfd6c
민경훈 <148615488+oneplast@users.noreply.github.com>
2025-01-17 10:26:10 +0900

    [level 2] Title: 튜플, Time: 79.61 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,35 +1,19 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(String s) {
-        List<Integer> resultList = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
-        String fullStr = s.substring(2, s.length() - 1);
 
-        String[] strs = fullStr.split(",\\{");
+        String[] strBits = s.replaceAll("[{}]", "").split(",");
 
-        for (String str : strs) {
-            str = str.substring(0, str.length() - 1);
-            for (String checkStr : str.split(",")) {
-                int key = Integer.parseInt(checkStr);
-                map.put(key, map.getOrDefault(key, 0) + 1);
-            }
+        for (String strBit : strBits) {
+            int key = Integer.parseInt(strBit);
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
-        Map<Integer, Integer> sortedMap = map.entrySet().stream()
+        return map.entrySet().stream()
                 .sorted((a, b) -> b.getValue() - a.getValue())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (a, b) -> a,
-                        LinkedHashMap::new
-                ));
-
-        for (Map.Entry<Integer, Integer> dataMap : sortedMap.entrySet()) {
-            resultList.add(dataMap.getKey());
-        }
-
-        return resultList.stream().mapToInt(x -> x).toArray();
+                .mapToInt(Map.Entry::getKey)
+                .toArray();
     }
 }
