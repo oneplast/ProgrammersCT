3418538988fda33632b6d9a7d18368b024eed6cf
민경훈 <148615488+oneplast@users.noreply.github.com>
2025-03-28 01:03:48 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.33 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -3,30 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int result = 0;
+        List<Deque<Integer>> list = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
-        int n = board.length;
+        for (int i = 0; i < board.length; i++) {
+            list.add(new ArrayDeque<>());
+        }
 
-        int j;
-        int cur;
-        for (int move : moves) {
-            j = move - 1;
-            cur = 0;
-
-            for (int i = 0; i < n; i++) {
-                if (board[i][j] > 0) {
-                    cur = board[i][j];
-                    board[i][j] = 0;
-                    break;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != 0) {
+                    list.get(j).offerLast(board[i][j]);
                 }
             }
+        }
 
-            if (cur > 0) {
-                if (!stack.isEmpty() && stack.peek() == cur) {
-                    stack.pop();
-                    result += 2;
-                } else {
-                    stack.push(cur);
-                }
+        int idx;
+        for (int i = 0; i < moves.length; i++) {
+            idx = moves[i] - 1;
+
+            if (list.get(idx).isEmpty()) {
+                continue;
+            }
+
+            int cur = list.get(idx).pollFirst();
+
+            if (!stack.isEmpty() && stack.peek() == cur) {
+                stack.pop();
+                result += 2;
+            } else {
+                stack.push(cur);
             }
         }
 
