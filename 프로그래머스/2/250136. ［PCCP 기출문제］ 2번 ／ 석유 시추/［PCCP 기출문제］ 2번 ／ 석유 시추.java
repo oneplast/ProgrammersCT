7327a32f649eda4c202487dc2415b44ca7f6f996
민경훈 <148615488+oneplast@users.noreply.github.com>
2025-06-22 00:03:22 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 93.62 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,79 +2,73 @@ import java.util.*;
 
 class Solution {
     int n, m;
-    int[][] land;
-    int[][] visited;
     int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    Map<Integer, Integer> oilSize = new HashMap<>();
+    int[][] dp;
 
     public int solution(int[][] land) {
-        this.n = land.length;
-        this.m = land[0].length;
-        this.land = land;
-        this.visited = new int[n][m];
+        n = land.length;
+        m = land[0].length;
+        dp = new int[n][m];
+        int max = Integer.MIN_VALUE;
+        Map<Integer, Integer> oilMap = new HashMap<>();
 
         int id = 1;
-
-        // BFS로 덩어리별 ID 부여 및 크기 저장
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (land[i][j] == 1 && visited[i][j] == 0) {
-                    int size = bfs(i, j, id);
-                    oilSize.put(id, size);
-                    id++;
+                if (land[i][j] == 1 && dp[i][j] == 0) {
+                    int weight = bfs(land, j, i, id);
+                    oilMap.put(id, weight);
+                    ++id;
                 }
             }
         }
 
-        int maxOil = 0;
+        for (int i = 0; i < m; i++) {
+            Set<Integer> visited = new HashSet<>();
 
-        // 각 열마다 아래로 훑어서 덩어리 ID 수집
-        for (int col = 0; col < m; col++) {
-            Set<Integer> touched = new HashSet<>();
-
-            for (int row = 0; row < n; row++) {
-                int oilId = visited[row][col];
+            for (int j = 0; j < n; j++) {
+                int oilId = dp[j][i];
                 if (oilId != 0) {
-                    touched.add(oilId);
+                    visited.add(oilId);
                 }
             }
 
-            int total = 0;
-            for (int oilId : touched) {
-                total += oilSize.get(oilId);
+            int sum = 0;
+            for (int oilId : visited) {
+                sum += oilMap.get(oilId);
             }
 
-            maxOil = Math.max(maxOil, total);
+            max = Math.max(max, sum);
         }
 
-        return maxOil;
+        return max;
     }
 
-    private int bfs(int x, int y, int id) {
+    private int bfs(int[][] land, int x, int y, int id) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        visited[x][y] = id;
+        queue.offer(new int[]{y, x});
+        dp[y][x] = id;
 
-        int count = 1;
-
+        int cnt = 1;
         while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int cx = curr[0], cy = curr[1];
+            int[] cur = queue.poll();
+            int curX = cur[1];
+            int curY = cur[0];
 
             for (int[] dir : dirs) {
-                int nx = cx + dir[0];
-                int ny = cy + dir[1];
+                int nextX = curX + dir[1];
+                int nextY = curY + dir[0];
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (land[nx][ny] == 1 && visited[nx][ny] == 0) {
-                        visited[nx][ny] = id;
-                        queue.offer(new int[]{nx, ny});
-                        count++;
+                if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
+                    if (land[nextY][nextX] == 1 && dp[nextY][nextX] == 0) {
+                        dp[nextY][nextX] = id;
+                        queue.offer(new int[]{nextY, nextX});
+                        ++cnt;
                     }
                 }
             }
         }
 
-        return count;
+        return cnt;
     }
 }
