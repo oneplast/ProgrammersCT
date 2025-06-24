20e35804adb651effc620446a9cb50044d5d4c03
민경훈 <148615488+oneplast@users.noreply.github.com>
2025-06-24 19:36:01 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 17.71 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        for (int i = 1; i <= r2; i++) {
+            int maxY = (int) getDistance(r2, i);
+            int minY = 0;
+            int minYDistance = (int) Math.ceil(getDistance(r1, i));
+            if (minYDistance > 0) {
+                minY = minYDistance;
+            }
+
+            answer += (maxY - minY + 1);
+        }
+
+        return answer * 4;
+    }
+
+    private double getDistance(long r, long y) {
+        return Math.sqrt(r * r - y * y);
+    }
+}
