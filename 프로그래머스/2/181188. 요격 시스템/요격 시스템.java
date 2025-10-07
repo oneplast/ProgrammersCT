c25a85f18e32e60615dd43541939858c13e67b58
민경훈 <148615488+oneplast@users.noreply.github.com>
2025-10-07 23:55:14 +0900

    [level 2] Title: 요격 시스템, Time: 430.00 ms, Memory: 182 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        int[][] sortedArr = Arrays.stream(targets)
+                .sorted((a, b) -> a[1] - b[1])
+                .toArray(int[][]::new);
+
+        double curValue = -1;
+        for (int[] target : sortedArr) {
+            int s = target[0];
+            int e = target[1];
+
+            if (curValue <= s) {
+                ++answer;
+                curValue = e - 0.1;
+            }
+        }
+
+        return answer;
+    }
+}
