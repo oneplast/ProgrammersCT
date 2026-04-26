d796735f4971df2b716c393409acac9f88436ee7
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-04-26 23:03:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    static final int MOD = 10_007;
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % MOD;
+        }
+
+        System.out.print(dp[n]);
+    }
+}
