daed255748e0559a689975d33511a5dd88da84d5
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-07-15 11:53:46 +0900

    [level 0] Title: 배열 만들기 2, Time: 2.13 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,32 +1,42 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
+    static final int MUL = 10;
+    static final int BASE = 5;
+    
+    static Queue<Integer> queue = new LinkedList<>();
+    static List<Integer> list = new ArrayList<>();
+    
     public int[] solution(int l, int r) {
-        List<Integer> list = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(5);
-
-        while (!q.isEmpty()) {
-            int data = q.poll();
-
-            if (data < l) {
-                q.offer(data * 10);
-                q.offer(data * 10 + 5);
-                continue;
-            }
-
-            if (data > r) {
-                continue;
-            }
-
-            list.add(data);
-
-            q.offer(data * 10);
-            q.offer(data * 10 + 5);
+        queue.offer(BASE);
+        
+        bfs(l, r);
+        
+        if (list.isEmpty()) {
+            return new int[]{-1};
         }
-
-        Collections.sort(list);
-
-        return list.isEmpty() ? new int[]{-1} : list.stream().mapToInt(x -> x).toArray();
+        
+        return list.stream()
+            .mapToInt(x -> x)
+            .toArray();
+    }
+    
+    private void bfs(int l, int r) {
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            
+            if (num > r) {
+                continue;
+            }
+            
+            if (num >= l) {
+                list.add(num);
+            }
+            
+            queue.offer(num * MUL);
+            queue.offer(num * MUL + BASE);
+        }
+        
     }
 }
