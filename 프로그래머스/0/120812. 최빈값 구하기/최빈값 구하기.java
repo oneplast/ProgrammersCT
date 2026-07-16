d49541ec76d025e37cbc06fc3e09be86beaa14ce
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-07-16 20:56:45 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,40 +1,25 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
+    static final int SIZE = 1000;
+    
     public int solution(int[] array) {
-        Map<Integer, Integer> map = Arrays.stream(array).boxed().collect(Collectors.toMap(
-                key -> key,
-                value -> 1,
-                Integer::sum
-        ));
-
-        Map<Integer, Integer> sortedMap = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (a, b) -> a,
-                        LinkedHashMap::new
-                ));
-
-        int key = 0;
-        int value = 0;
-        for (Map.Entry<Integer, Integer> entry : sortedMap.entrySet()) {
-            if (key == 0 && value == 0) {
-                key = entry.getKey();
-                value = entry.getValue();
-                continue;
-            }
-
-            if (value == entry.getValue()) {
-                return -1;
-            } else {
-                return key;
+        int[] counts = new int[SIZE];
+        for (int i = 0; i < array.length; i++) {
+            counts[array[i]]++;
+        }
+        
+        boolean isMulti = false;
+        int max = 0, result = 0;
+        for (int i = 0; i < counts.length; i++) {
+            int cur = counts[i];
+            if (cur > max) {
+                isMulti = false;
+                max = cur;
+                result = i;
+            } else if (cur == max) {
+                isMulti = true;
             }
         }
-
-        return key;
+        
+        return isMulti ? -1 : result;
     }
 }
