87d10a9a5639509a96495bd5365d98e197c4c503
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-08-19 09:42:14 +0900

    [level 1] Title: 약수의 합, Time: 2.53 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,9 @@
+import java.util.stream.*;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for (int i=1; i<=n; i++) {
-            if (n%i==0) answer += i;
-        }
-        return answer;
+        return IntStream.rangeClosed(1, n)
+                .filter(i -> n % i == 0)
+                .sum();
     }
 }
