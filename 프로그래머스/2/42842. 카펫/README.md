ff424511137397bca9efa958bfcc9c259de073ff
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-09-03 13:29:09 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 450 MB, 시간: 569.89 ms
+메모리: 89.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 23일 10:17:17
+2026년 09월 03일 13:29:08
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,50 +1,16 @@
-import java.util.*;
-
 class Solution {
-    int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
     public int[] solution(int brown, int yellow) {
-        int nm = brown + yellow;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 3; i <= nm / 2; i++) {
-            if (nm % i == 0 && !list.contains(i) && (nm / i) >= 3) {
-                list.add(i);
-                list.add(nm / i);
-            }
-        }
+        int totalSize = brown + yellow;
 
-        int idx = 0;
-        while (idx < list.size()) {
-            int n = list.get(idx);
-            int m = idx % 2 == 0 ? list.get(idx + 1) : list.get(idx - 1);
-            int[][] board = new int[n][m];
-            board[0][0] = 1;
-            int dir = 0;
-
-            int x = 0;
-            int y = 0;
-            while (true) {
-                x += dirs[dir][1];
-                y += dirs[dir][0];
-
-                if (x >= 0 && x < m && y >= 0 && y < n) {
-                    board[y][x] = 1;
-                } else {
-                    x -= dirs[dir][1];
-                    y -= dirs[dir][0];
-                    dir = (dir + 1) % dirs.length;
-                    if (dir == 0) {
-                        break;
-                    }
-                }
+        for (int row = 3; row <= Math.sqrt(totalSize); row++) {
+            if (totalSize % row != 0) {
+                continue;
             }
 
-            int cnt = (int) Arrays.stream(board).flatMapToInt(Arrays::stream).filter(a -> a == 1).count();
+            int col = totalSize / row;
 
-            if (cnt == brown) {
-                return new int[]{m, n};
-            } else {
-                idx++;
+            if (2 * (row + col) - 4 == brown) {
+                return new int[]{col, row};
             }
         }
 
