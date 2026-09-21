17317eadbef64bb7e95e1086eb3471ec30c5b5b5
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-09-21 21:48:20 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,13 +1,15 @@
 class Solution {
-    public long solution(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+	static final int MOD = 1_234_567;
 
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
-        }
+	public long solution(int n) {
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
 
-        return dp[n];
-    }
+		for (int i = 2; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+		}
+
+		return dp[n];
+	}
 }
