dfadffbc33a3662aed5a778ae71812b3d2f7b1b5
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-09-23 22:58:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,37 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int n, String[] words) {
-        Set<String> set = new HashSet<>();
-        Stack<String> stack = new Stack<>();
-        Queue<String> q = new LinkedList<>();
-        Arrays.stream(words).forEach(q::offer);
+	public int[] solution(int n, String[] words) {
+		Set<String> wordSet = new HashSet<>();
+		wordSet.add(words[0]);
 
-        int num = 1;
-        int turn = 0;
-        while (turn < words.length && !q.isEmpty()) {
-            String cur = q.poll();
+		for (int i = 1; i < words.length; i++) {
+			if (!wordSet.add(words[i])
+				|| words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1)
+			) {
+				return new int[] {(i % n) + 1, (i + n) / n};
+			}
+		}
 
-            if (!set.contains(cur)) {
-                if (!stack.isEmpty()) {
-                    String lastStr = stack.peek();
-                    if (lastStr.charAt(lastStr.length() - 1) != cur.charAt(0)) {
-                        return new int[]{turn % n + 1, num};
-                    }
-                }
-
-                set.add(cur);
-                stack.push(cur);
-                turn++;
-
-                if (turn / n == num) {
-                    num++;
-                }
-            } else {
-                return new int[]{turn % n + 1, num};
-            }
-        }
-
-        return new int[]{0, 0};
-    }
+		return new int[] {0, 0};
+	}
 }
