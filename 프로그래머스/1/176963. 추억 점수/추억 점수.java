f13c775573204329067a7bb9f65561f708b091a4
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-09-24 21:57:14 +0900

    [level 1] Title: 추억 점수, Time: 9.29 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -3,20 +3,15 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] results = new int[photo.length];
         Map<String, Integer> scoreBoard = new HashMap<>();
         IntStream.range(0, name.length)
                 .forEach(i -> scoreBoard.put(name[i], yearning[i]));
 
-        int resultIdx = 0;
-        for (String[] photoNames : photo) {
-            int photoScore = 0;
-            for (String photoName : photoNames) {
-                photoScore += scoreBoard.getOrDefault(photoName, 0);
-            }
-            results[resultIdx++] = photoScore;
-        }
-
-        return results;
+        return Arrays.stream(photo)
+                .mapToInt(photoNames ->
+                        Arrays.stream(photoNames)
+                                .mapToInt(photoName -> scoreBoard.getOrDefault(photoName, 0))
+                                .sum())
+                .toArray();
     }
 }
